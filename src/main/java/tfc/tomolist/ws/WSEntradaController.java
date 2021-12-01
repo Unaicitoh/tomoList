2aec038b15b833e5ab1e7da0ebc8280e22ae0540
Unai Gonzalez <unaigsanchez@gmail.com>
2021-12-01 20:41:51 +0100

    Estilo perfil, repositorio solicitudes

--- src/main/java/tfc/tomolist/ws/WSEntradaController.java
+++ src/main/java/tfc/tomolist/ws/WSEntradaController.java
@@ -3,10 +3,6 @@ package tfc.tomolist.ws;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.web.configurers.SecurityContextConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
