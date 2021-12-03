4f7cbb885b5c4075f6d63ad3c3c4b630593d6e47
Unai Gonzalez <unaigsanchez@gmail.com>
2021-12-03 20:04:54 +0100

    ajuste heroku

---
+++ src/main/resources/data.sql
@@ -0,0 +1,87 @@
+INSERT INTO roles values (1,"ROLE_ADMIN");
+INSERT INTO roles values (2,"ROLE_ESTANDAR");
+
+INSERT INTO usuarios values (0,"No sólo sobrevivió 500 con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.","Calle 13",22,"u@email.com","2021/05/20","https://images-na.ssl-images-amazon.com/images/I/610FlSOYxiL._AC_SX522_.jpg","H","Unai Gonzalez","$2y$10$8zHvivHClP.zLVJB0kwBLe4/XmbfR.HVZx2US.fF8X/Uf318cYOEi","123","666","usu1",2);
+INSERT INTO usuarios values (0,"Adivinas","Calle 13",22,"r@email.com","2021/05/20","https://statics.proyectoclubes.com/images/sporting/opengraph_image.png?2","H","Roberto Rodriguez","$2y$10$8zHvivHClP.zLVJB0kwBLe4/XmbfR.HVZx2US.fF8X/Uf318cYOEi","123","662","usu2",2);
+INSERT INTO usuarios values (0,"Adivinas","Calle 13",22,"x@email.com","2021/05/20","https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/594a1ced5bafe85dfd3c9869/gato-romano_0.jpg","M","Xenia Rodriguez","$2y$10$8zHvivHClP.zLVJB0kwBLe4/XmbfR.HVZx2US.fF8X/Uf318cYOEi","123","661","usu3",2);
+INSERT INTO usuarios values (0,"","",16,"admin@email.com","2021/05/20","","H","Administrador","$2a$10$vk1o5JMQqAIF1EV7L2j3kePhOOlRI84FK51XhyBRt5ITv85UIuOde","admin","","admin",1);
+
+INSERT INTO amigos values (0,1,1,2);
+INSERT INTO amigos values (0,0,1,3);
+INSERT INTO amigos values (0,1,2,3);
+
+INSERT INTO mensajes values (0,"Prueba","2021/05/20 12:50:30",1,2);
+INSERT INTO mensajes values (0,"Prueba 2","2021/05/20 12:51:24",2,1);
+INSERT INTO mensajes values (0,"Prueba 3","2021/05/20 12:52:54",1,2);
+INSERT INTO mensajes values (0,"Prueba 4","2021/05/20 12:53:15",1,2);
+INSERT INTO mensajes values (0,"Prueba 5","2021/05/20 12:54:34",2,1);
+
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/03/25 11:50:34","https://images.pexels.com/photos/5767607/pexels-photo-5767607.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Entrada1",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/20 12:22:34","https://images.pexels.com/photos/7047538/pexels-photo-7047538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Entrada2",1);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget lobortis quam. Nullam gravida tempus ultricies. Maecevolutpat. ","2021/05/20 12:30:34","https://images.pexels.com/photos/7196477/pexels-photo-7196477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Entrada3",1);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit amet, consectetur a. Aliquam erat volutpat. ","2021/05/20 05:23:34","https://images.pexels.com/photos/735911/pexels-photo-735911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titular",2);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/11 15:58:34","https://images.pexels.com/photos/6156972/pexels-photo-6156972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Ejemplisimo",3);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit amet, consectetur a. Aliquam erat volutpat. ","2021/05/13 03:14:34","https://images.pexels.com/photos/533446/pexels-photo-533446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titulardo",2);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/16 14:58:34","https://images.pexels.com/photos/2694434/pexels-photo-2694434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Fotito",3);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit amet, consectetur a. Aliquam erat volutpat. ","2021/05/20 15:12:34","https://images.pexels.com/photos/9183256/pexels-photo-9183256.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titulador",2);
+INSERT INTO entradas values (0,"Lorem ipsum dolor sit quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/15 08:58:34","https://images.pexels.com/photos/10095818/pexels-photo-10095818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Hola Mundo",3);
+
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/08/13 04:45:34","https://images.pexels.com/photos/1161547/pexels-photo-1161547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Entrada1",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/03/25 20:15:34","https://images.pexels.com/photos/3812048/pexels-photo-3812048.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Adivinamelo",1);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/06/25 01:10:34","https://images.pexels.com/photos/973506/pexels-photo-973506.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Ay no que",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/03/09 23:11:34","https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Pepe fulanito",1);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/08/23 04:10:34","https://images.pexels.com/photos/1918290/pexels-photo-1918290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","En plan",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/02/03 18:14:34","https://images.pexels.com/photos/165236/pexels-photo-165236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titulazo",1);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/07/26 15:10:34","https://images.pexels.com/photos/247676/pexels-photo-247676.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500","Casa",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/14 11:43:34","https://images.pexels.com/photos/34225/spider-web-with-water-beads-network-dewdrop.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titulo entrada largo",1);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/01/14 12:45:34","https://images.pexels.com/photos/2002719/pexels-photo-2002719.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Limpa baño",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/02/20 08:22:34","https://images.pexels.com/photos/270700/pexels-photo-270700.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Titulo entrada",1);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/14 11:43:34","https://images.pexels.com/photos/3819818/pexels-photo-3819818.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","XXFFFFFA",1);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/06/25 01:10:34","https://images.pexels.com/photos/2603464/pexels-photo-2603464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","XADSA",1);
+
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/06/20 15:30:34","https://images.pexels.com/photos/2570139/pexels-photo-2570139.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Lava los platos",2);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/01/13 16:30:34","https://images.pexels.com/photos/3571065/pexels-photo-3571065.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","DataEjemplo",2);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/02/20 23:45:34","https://images.pexels.com/photos/4394349/pexels-photo-4394349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Botellon",3);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/10/12 14:14:34","https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Practica",3);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/07/20 21:22:34","https://images.pexels.com/photos/1308713/pexels-photo-1308713.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","MoonToGo",2);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/01/20 02:15:34","https://images.pexels.com/photos/177809/pexels-photo-177809.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Posteao",2);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/06/13 14:10:34","https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Entrada titulo ejemplo",3);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/10 12:30:34","https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","EjemploPost",3);
+INSERT INTO entradas values (0,"Lores sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/03/10 04:45:34","https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","Estaagauy",2);
+INSERT INTO entradas values (0,"Sed ultricies, felis ut laoreet tincidunt, ipsum quam facilisis nibh, sed fringilla arcu diam sit amet eros. Quisque sapien turpis, tristique et ornare facilisis, tempor ut nunc. Aliquam erat volutpat. ","2021/05/20 15:22:34","https://images.pexels.com/photos/1831236/pexels-photo-1831236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940","ADivinas",2);
+
+
+INSERT INTO megustas values (0,1,1);
+INSERT INTO megustas values (0,2,1);
+INSERT INTO megustas values (0,3,1);
+INSERT INTO megustas values (0,26,2);
+INSERT INTO megustas values (0,26,3);
+INSERT INTO megustas values (0,25,1);
+INSERT INTO megustas values (0,25,2);
+INSERT INTO megustas values (0,25,3);
+INSERT INTO megustas values (0,6,2);
+INSERT INTO megustas values (0,6,2);
+INSERT INTO megustas values (0,7,2);
+INSERT INTO megustas values (0,7,1);
+INSERT INTO megustas values (0,8,3);
+INSERT INTO megustas values (0,8,2);
+INSERT INTO megustas values (0,9,3);
+INSERT INTO megustas values (0,10,2);
+INSERT INTO megustas values (0,10,3);
+
+INSERT INTO comentarios values (0,"Buenaaas","2021/05/20 12:54:34",null,1,1);
+INSERT INTO comentarios values (0,"QUe tal tio","2021/05/20 12:56:34",1,1,2);
+INSERT INTO comentarios values (0,"Eyy","2021/05/20 12:57:34",1,1,3);
+
+INSERT INTO comentarios values (0,"Buenaaas","2021/05/20 12:54:34",null,1,1);
+INSERT INTO comentarios values (0,"QUe tal tio","2021/05/20 12:56:34",1,25,2);
+INSERT INTO comentarios values (0,"assdsad tio","2021/05/24 12:56:34",1,25,2);
+INSERT INTO comentarios values (0,"Eyy","2021/05/20 12:57:34",null,26,3);
+
+INSERT INTO comentarios values (0,"Buenaaas","2021/05/20 12:54:34",null,1,1);
+INSERT INTO comentarios values (0,"Pepeeee","2021/05/20 12:52:34",1,25,3);
+INSERT INTO comentarios values (0,"Eyy","2021/05/20 12:57:34",1,26,3);
+
+INSERT INTO comentarios values (0,"Buenaaas","2021/05/20 12:54:34",null,26,1);
+INSERT INTO comentarios values (0,"QUe tal tio","2021/05/20 12:56:34",1,26,2);
+INSERT INTO comentarios values (0,"Eyy","2021/05/20 12:57:34",1,26,3);
